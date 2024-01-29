63349179ac16d02bc37163f190ed0cb8154e9eca
Kandie Kip <hkandie@sourceallies.com>
2024-01-29 09:32:38 -0600

    wip

--- app/consumer.py
+++ app/consumer.py
@@ -3,7 +3,8 @@ import json
 consumer = KafkaConsumer(
     'users',
     bootstrap_servers=['localhost:9093'],
-    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    group_id='my-group-1'
 )
 # note that this for loop will block forever to wait for the next message
 for message in consumer:
--- app/producer.py
+++ app/producer.py
@@ -9,3 +9,4 @@ producer = KafkaProducer(
 fake = Faker()
 rs=producer.send('users', {'author': fake.user_name(), 'content': fake.paragraph(nb_sentences=5), 'created_at': datetime.now().isoformat()})
 print(rs.get(timeout=60))
+print(producer.to_string())
